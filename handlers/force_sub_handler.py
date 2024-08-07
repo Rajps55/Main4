ae74f8a3a361a117b6b0950fb0558c406a7b523c
Rajps55 <hackinggm69@gmail.com>
2024-08-07 22:08:32 +0530

    Update force_sub_handler.py

--- handlers/force_sub_handler.py
+++ handlers/force_sub_handler.py
@@ -47,7 +47,7 @@ async def handle_force_sub(bot: Client, cmd: Message):
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("🤖 Join Updates Channel","https://t.me/netfilixmo_ch" )
+                        InlineKeyboardButton("🤖 Join Updates Channel",url=invite_link.invite_link)
                     ],
                     [
                         InlineKeyboardButton("🔄 Refresh 🔄", callback_data="refreshForceSub")
