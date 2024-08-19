1caf70b4007f182a9550857815ab7137828b64db
Rajps55 <hackinggm69@gmail.com>
2024-08-19 17:05:05 +0530

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -1,7 +1,4 @@
 import asyncio
-import requests
-import string
-import random
 from configs import Config
 from pyrogram import Client
 from pyrogram.types import Message
@@ -21,16 +18,15 @@ async def reply_forward(message: Message, file_id: int):
 
 async def media_forward(bot: Client, user_id: int, file_id: int):
     try:
-        if Config.FORWARD_AS_COPY is True:
+        if Config.FORWARD_AS_COPY:
             return await bot.copy_message(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
                                           message_id=file_id)
-        elif Config.FORWARD_AS_COPY is False:
+        else:
             return await bot.forward_messages(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
                                               message_ids=file_id)
     except FloodWait as e:
         await asyncio.sleep(e.value)
-        return media_forward(bot, user_id, file_id)
-        await message.delete()
+        return await media_forward(bot, user_id, file_id)
 
 async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
     sent_message = await media_forward(bot, user_id, file_id)
