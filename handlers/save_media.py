420536a7ecf20208d0f28d5f32ca252da6bb7ac2
Rajps55 <hackinggm69@gmail.com>
2024-08-20 00:42:25 +0530

    Update save_media.py

--- handlers/save_media.py
+++ handlers/save_media.py
@@ -3,9 +3,8 @@ from pyrogram import Client
 from pyrogram.types import Message
 from pyrogram.errors import FloodWait
 from configs import Config
-from handlers.helpers import str_to_b64
 
-async def save_media_in_channel(bot: Client, message: Message):
+async def save_media_in_channel(bot: Client, message: Message, retries=5):
     try:
         if Config.SAVE_MEDIA_AS_COPY:
             return await bot.copy_message(
@@ -20,24 +19,12 @@ async def save_media_in_channel(bot: Client, message: Message):
                 message_ids=message.message_id
             )
     except FloodWait as e:
-        await asyncio.sleep(e.value)
-        return await save_media_in_channel(bot, message)
+        if retries > 0:
+            await asyncio.sleep(e.value)
+            return await save_media_in_channel(bot, message, retries=retries-1)
+        else:
+            print(f"Failed to save media after multiple retries: {e}")
 
 async def save_batch_media_in_channel(bot: Client, messages: list):
     for message in messages:
-        try:
-            if Config.SAVE_MEDIA_AS_COPY:
-                await bot.copy_message(
-                    chat_id=Config.DB_CHANNEL,
-                    from_chat_id=message.chat.id,
-                    message_id=message.message_id
-                )
-            else:
-                await bot.forward_messages(
-                    chat_id=Config.DB_CHANNEL,
-                    from_chat_id=message.chat.id,
-                    message_ids=message.message_id
-                )
-        except FloodWait as e:
-            await asyncio.sleep(e.value)
-            await save_batch_media_in_channel(bot, messages)
+        await save_media_in_channel(bot, message)
