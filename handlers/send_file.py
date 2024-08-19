6085fd33faef0245c26ae8bc60120a62d245d653
Rajps55 <hackinggm69@gmail.com>
2024-08-20 00:44:49 +0530

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -3,20 +3,22 @@ from configs import Config
 from pyrogram import Client
 from pyrogram.types import Message
 from pyrogram.errors import FloodWait
-from handlers.helpers import str_to_b64
 
-async def reply_forward(message: Message, file_id: int):
+async def reply_forward(message: Message, file_id: int, retries=5):
     try:
         await message.reply_text(
-            f"Files will be deleted in 30 minutes to avoid copyright issues. Please forward and save them.",
+            "Files will be deleted in 30 minutes to avoid copyright issues. Please forward and save them.",
             disable_web_page_preview=True,
             quote=True
         )
     except FloodWait as e:
-        await asyncio.sleep(e.x)
-        await reply_forward(message, file_id)
+        if retries > 0:
+            await asyncio.sleep(e.x)
+            await reply_forward(message, file_id, retries=retries-1)
+        else:
+            print(f"Failed to reply after multiple retries: {e}")
 
-async def media_forward(bot: Client, user_id: int, file_id: int):
+async def media_forward(bot: Client, user_id: int, file_id: int, retries=5):
     try:
         if Config.FORWARD_AS_COPY:
             return await bot.copy_message(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
@@ -25,8 +27,11 @@ async def media_forward(bot: Client, user_id: int, file_id: int):
             return await bot.forward_messages(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
                                               message_ids=file_id)
     except FloodWait as e:
-        await asyncio.sleep(e.value)
-        return await media_forward(bot, user_id, file_id)
+        if retries > 0:
+            await asyncio.sleep(e.value)
+            return await media_forward(bot, user_id, file_id, retries=retries-1)
+        else:
+            print(f"Failed to forward media after multiple retries: {e}")
 
 async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
     sent_message = await media_forward(bot, user_id, file_id)
