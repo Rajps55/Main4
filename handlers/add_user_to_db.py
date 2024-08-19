d3559e11eacb27aca05e95583b6ca3e1b775ef76
Rajps55 <hackinggm69@gmail.com>
2024-08-19 17:04:01 +0530

    Update add_user_to_db.py

--- handlers/add_user_to_db.py
+++ handlers/add_user_to_db.py
@@ -5,12 +5,9 @@ from pyrogram.types import Message
 
 async def add_user_to_database(bot: Client, cmd: Message):
     try:
-        # Check if the user is already in the database
         if not await db.is_user_exist(cmd.from_user.id):
-            # Add the new user to the database
             await db.add_user(cmd.from_user.id)
             
-            # Check if LOG_CHANNEL is set in the config
             if Config.LOG_CHANNEL:
                 try:
                     await bot.send_message(
@@ -21,4 +18,3 @@ async def add_user_to_database(bot: Client, cmd: Message):
                     print(f"Failed to send message to log channel: {e}")
     except Exception as e:
         print(f"Error in add_user_to_database: {e}")
-        # Optionally, you can also send a message to a specific admin or log channel about the error
