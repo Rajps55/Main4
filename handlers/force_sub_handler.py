116ec5d3cbafb1a58756972be84eda8bdf1d7ab6
Rajps55 <hackinggm69@gmail.com>
2024-08-19 17:07:39 +0530

    Update force_sub_handler.py

--- handlers/force_sub_handler.py
+++ handlers/force_sub_handler.py
@@ -1,12 +1,9 @@
 import asyncio
-from typing import (
-    Union
-)
+from typing import Union
 from configs import Config
 from pyrogram import Client
 from pyrogram.errors import FloodWait, UserNotParticipant
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
-
 
 async def get_invite_link(bot: Client, chat_id: Union[str, int]):
     try:
@@ -16,7 +13,6 @@ async def get_invite_link(bot: Client, chat_id: Union[str, int]):
         print(f"Sleep of {e.value}s caused by FloodWait ...")
         await asyncio.sleep(e.value)
         return await get_invite_link(bot, chat_id)
-
 
 async def handle_force_sub(bot: Client, cmd: Message):
     if Config.UPDATES_CHANNEL and Config.UPDATES_CHANNEL.startswith("-100"):
@@ -30,37 +26,10 @@ async def handle_force_sub(bot: Client, cmd: Message):
         if user.status == "kicked":
             await bot.send_message(
                 chat_id=cmd.from_user.id,
-                text="Sorry Sir, You are Banned to use me. Contact my [𝙎𝙪𝙥𝙥𝙤𝙧𝙩 𝙂𝙧𝙤𝙪𝙥](https://t.me/Netfilix_movie_shaport).",
+                text="Sorry Sir, You are banned from using me. Contact my [Support Group](https://t.me/Netfilix_movie_shaport).",
                 disable_web_page_preview=True
             )
             return 400
     except UserNotParticipant:
         try:
-            invite_link = await get_invite_link(bot, chat_id=channel_chat_id)
-        except Exception as err:
-            print(f"Unable to do Force Subscribe to {Config.UPDATES_CHANNEL}\n\nError: {err}")
-            return 200
-        await bot.send_message(
-            chat_id=cmd.from_user.id,
-            text="**Please Join My Updates Channel to use this Bot!**\n\n"
-                 "Due to Overload, Only Channel Subscribers can use this Bot!",
-            reply_markup=InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton("🤖 Join Updates Channel",url=invite_link.invite_link)
-                    ],
-                    [
-                        InlineKeyboardButton("🔄 Refresh 🔄", callback_data="refreshForceSub")
-                    ]
-                ]
-            )
-        )
-        return 400
-    except Exception:
-        await bot.send_message(
-            chat_id=cmd.from_user.id,
-            text="Something went Wrong. Contact my [𝙎𝙪𝙥𝙥𝙤𝙧𝙩 𝙂𝙧𝙤𝙪𝙥](https://t.me/Netfilix_movie_shaport).",
-            disable_web_page_preview=True
-        )
-        return 200
-    return 200
+            invite_link = await get_invite_link(bot, chat_id=channel
