e846f0b89a3abdfe6d91c9ee2feb89906013339d
Rajps44 <singhayush88805@gmail.com>
2024-08-25 20:38:34 +0530

    Update check_user_status.py

--- handlers/check_user_status.py
+++ handlers/check_user_status.py
@@ -1,41 +1,26 @@
 import datetime
 from configs import Config
-from handlers.database import db
+from handlers.database import Database
+
+db = Database(Config.DATABASE_URL, Config.BOT_USERNAME)
+
 
 async def handle_user_status(bot, cmd):
     chat_id = cmd.from_user.id
-    try:
-        if not await db.is_user_exist(chat_id):
-            await db.add_user(chat_id)
-            if Config.LOG_CHANNEL:
-                await bot.send_message(
-                    Config.LOG_CHANNEL,
-                    f"#NEW_USER: \n\nNew User [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id}) started @{Config.BOT_USERNAME} !!"
-                )
-    except Exception as e:
-        print(f"Error in handle_user_status: {e}")
+    if not await db.is_user_exist(chat_id):
+        await db.add_user(chat_id)
         await bot.send_message(
             Config.LOG_CHANNEL,
-            f"#ERROR_TRACEBACK:\nError in handle_user_status: {e}"
+            f"#NEW_USER: \n\nNew User [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id}) started @{Config.BOT_USERNAME} !!"
         )
-        return
 
-    try:
-        ban_status = await db.get_ban_status(chat_id)
-        if ban_status["is_banned"]:
-            if (
-                    datetime.date.today() - datetime.date.fromisoformat(ban_status["banned_on"])
-            ).days > ban_status["ban_duration"]:
-                await db.remove_ban(chat_id)
-            else:
-                await cmd.reply_text("You are banned! Contact @VJ_Botz 😝", quote=True)
-                return
-    except Exception as e:
-        print(f"Error checking ban status: {e}")
-        await bot.send_message(
-            Config.LOG_CHANNEL,
-            f"#ERROR_TRACEBACK:\nError checking ban status: {e}"
-        )
-        return
-
+    ban_status = await db.get_ban_status(chat_id)
+    if ban_status["is_banned"]:
+        if (
+                datetime.date.today() - datetime.date.fromisoformat(ban_status["banned_on"])
+        ).days > ban_status["ban_duration"]:
+            await db.remove_ban(chat_id)
+        else:
+            await cmd.reply_text("You R Banned!.. Contact @VJ_Botz 😝", quote=True)
+            return
     await cmd.continue_propagation()
