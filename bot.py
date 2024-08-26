b3a5dd45352a87c11e89453148e94ee24bd69bed
Rajps44 <singhayush88805@gmail.com>
2024-08-26 17:27:06 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -84,7 +84,7 @@ async def start(bot: Client, cmd: Message):
                     [
                         InlineKeyboardButton("Support Group", url="https://t.me/Netfilix_movie_shaport")
                         
-                    ]
+                    ])
         
     else:
         try:
