5f3f9b63a1f739a87a90ae6c8a2ad9f7dc60b621
Rajps55 <hackinggm69@gmail.com>
2024-09-05 13:34:49 +0530

    Update rename.py

--- handlers/rename/rename.py
+++ handlers/rename/rename.py
@@ -4,7 +4,7 @@ from asyncio import sleep
 from pyrogram import Client, filters, enums
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceReply, CallbackQuery
 from pyrogram.errors import FloodWait
-from config import RENAME_MODE
+from configs import RENAME_MODE
 import humanize
 import random
 
