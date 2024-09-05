86f36adb19385f5f2a0691913a70048daa3b8b1b
Rajps55 <hackinggm69@gmail.com>
2024-09-05 13:34:03 +0530

    Update cb_data.py

--- handlers/rename/cb_data.py
+++ handlers/rename/cb_data.py
@@ -4,7 +4,6 @@ from pyrogram import Client, filters
 from pyrogram.types import (  InlineKeyboardButton, InlineKeyboardMarkup,ForceReply)
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
-from database.users_chats_db import db
 import os 
 import humanize
 from PIL import Image
